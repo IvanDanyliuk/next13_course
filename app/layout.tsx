3b6cbb01a4380d2600b342ef43a01df57548422c
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-07-14 14:58:54 +0300

    Added basic layout, implemented Navbar and Footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,24 @@
-const Layout = () => {
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+export const metadata = {
+  title: 'Flexible',
+  description: 'Showcase and discover remarcable developer projects'
+}
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>layout</div>
+    <html lang='en'>
+      <body>
+        <Navbar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
   );
 };
 
-export default Layout;
+export default RootLayout;
